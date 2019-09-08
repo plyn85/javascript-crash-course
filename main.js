c94d9797e406c8c5b8e830bb44298d1549b072a3
plyn85 <plyn99@gmail.com>
2019-09-08 14:32:56 +0100

    selecting elements in the dom

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-// constructor function
+// single element 
+// const form = document.querySelector("h1")
+// form.innerHTML ="js for losers"
+// console.log(form)
+// // multiple element 
+// const list = document.querySelectorAll(".item");
 
-function Person (firstName,lastName,dob){
-this.firstName = firstName;
-this.lastName = lastName;
- this.dob = new Date(dob); //date object
-}
 
-// Instantiate object
-const person1 = new Person('John', 'doe', '2-16-1985'); 
-const person2 = new Person('Jane', 'doe', '2-26-1982'); 
-const person3 = new Person('Jim', 'doe', '23-26-1765'); 
-const person4 = new Person('James', 'doe', '23-16-1976'); 
+// list.forEach((item)=>console.log(item))
 
-console.log(person2.dob.getFullYear());
+const list = document.querySelector('.items')
+console.log(list)
+// list.remove();
+// list.lastElementChild.remove();
+list.firstElementChild.textContent = 'i love coding';
+list.lastElementChild.textContent = 'i hate coding';
+list.children[1].innerText = 'coding is for life not just for xmas';
+list.children[1].innerHTML =' <h1>Code Code Code</h1>';
+const btn = document.querySelector(".btn");
+btn.style.background = 'red';
+console.log(btn)
+const mainHeader = document.querySelector('h1');
+mainHeader.style.background ="red";
+console.log(mainHeader)
