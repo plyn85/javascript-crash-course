13ae399d1f2ef7b1fde995c6007d55d2b4674176
plyn85 <plyn99@gmail.com>
2019-09-08 16:51:40 +0100

    adding event listner for submit button

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
-// single element 
-// const form = document.querySelector("h1")
-// form.innerHTML ="js for losers"
-// console.log(form)
-// // multiple element 
-// const list = document.querySelectorAll(".item");
 
-
-// list.forEach((item)=>console.log(item))
-
-const list = document.querySelector('.items')
-console.log(list)
-// list.remove();
-// list.lastElementChild.remove();
-list.firstElementChild.textContent = 'i love coding';
-list.lastElementChild.textContent = 'i hate coding';
-list.children[1].innerText = 'coding is for life not just for xmas';
-list.children[1].innerHTML =' <h1>Code Code Code</h1>';
 const btn = document.querySelector(".btn");
-btn.style.background = 'red';
-console.log(btn)
-const mainHeader = document.querySelector('h1');
-mainHeader.style.background ="red";
-console.log(mainHeader)
+
+btn.addEventListener('mouseover', (e) =>{
+    e.preventDefault();
+    document.querySelector("#my-form").style.background = "#ccc";
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').innerHTML = '<h1>hello coders </h2>';
+})
