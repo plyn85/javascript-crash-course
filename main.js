ed7ed0d7e111bd63de53b2a85c17b9f6b8e87ff1
plyn85 <plyn99@gmail.com>
2019-09-09 14:25:17 +0100

    form validation in javascript

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 
-const btn = document.querySelector(".btn");
+const myForm = document.querySelector('#my-form');
+const myName = document.querySelector('#name');
+const myEmail = document.querySelector('#email');
+const msg= document.querySelector('.msg');
+const user = document.querySelector('#users');
 
-btn.addEventListener('mouseover', (e) =>{
-    e.preventDefault();
-    document.querySelector("#my-form").style.background = "#ccc";
-    document.querySelector('body').classList.add('bg-dark');
-    document.querySelector('.items').innerHTML = '<h1>hello coders </h2>';
-})
+myForm.addEventListener('submit',onSubmit);
+
+function onSubmit (e) {
+e.preventDefault();
+
+if(myName.value === "" || myEmail.value === ""){
+ msg.classList.add("error");   
+msg.innerHTML = "please fill out required field";
+
+setTimeout(() => msg.remove(), 3000)
+}else{
+const li = document.createElement('li');
+li.appendChild(document.createTextNode(`${myName.value}: ${myEmail.value}`))
+user.appendChild(li);
+// clear fields
+myName.value = "";
+myEmail.value = "";
+}
+}
